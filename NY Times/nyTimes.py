e0f152f36fe1f283ab1a234c149906dc05482976
nolooker <121871676+nolooker@users.noreply.github.com>
2024-11-24 18:57:37 +0900

    nyTimes

--- NY Times/nyTimes.py
+++ NY Times/nyTimes.py
@@ -8,9 +8,8 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 import requests
 
 # API 키와 엔드포인트 설정
-app_id =  '98544a82-3e73-4183-b2cd-6b32b5212841'
 api_key = 'St24iaoXT67tzOIgB5sB4bEYWGxyrVly'
-secret_key = 'wgPn3wUtQE41keQA'
+
 url = f'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key={api_key}'
 
 # API 요청
@@ -20,5 +19,6 @@ if response.status_code == 200:
     for article in data['results']:
         print(f"Title: {article['title']}")
         print(f"URL: {article['url']}\n")
+        
 else:
     print(f"Failed to fetch data. Status code: {response.status_code}")
